bff01d14abd4dd460110cd6e48073790dea08d72
Erik Magnusson <contact@erikmagnusson.com>
2022-11-15 15:39:49 +0100

    Switched out Leaflet wrapper, no functionality implemented.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
-using FisSst.BlazorMaps.DependencyInjection;
 using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,7 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-builder.Services.AddBlazorLeafletMaps();
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
