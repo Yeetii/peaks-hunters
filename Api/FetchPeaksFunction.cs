7af3f8917f36120fb11bf675a028702efa69f787
Erik Magnusson <contact@erikmagnusson.com>
2022-11-25 14:18:37 +0100

    Extracted function for fetching Overpass

--- Api/FetchPeaksFunction.cs
+++ Api/FetchPeaksFunction.cs
@@ -10,6 +10,9 @@ using System.Text.Json.Serialization;
 using System.Text.Json;
 using System.Linq;
 using System.Net.Http;
+using BlazorApp.Shared;
+using System.Globalization;
+
 
 
 
@@ -61,15 +64,11 @@ namespace BlazorApp.Api
             )]IAsyncCollector<dynamic> documentsOut,
             ILogger log)
         {
-            var body = @"[out:json][timeout:25];" + "\n" +
-            @"            node[""natural""=""peak""](62.50826064422815,10.733642578125,64.3755654235413,15.4742431640625);" + "\n" +
-            @"            out body;" + "\n" +
-            @"            >;" + "\n" +
-            @"            out skel qt;";
-            var buffer = System.Text.Encoding.UTF8.GetBytes(body);
-            var byteContent = new ByteArrayContent(buffer);
-            var response = await client.PostAsync("https://overpass-api.de/api/interpreter", byteContent);
-            string rawPeaks = await response.Content.ReadAsStringAsync();
+            string peaksQuery = @"node[""natural""=""peak""]";
+            Coordinate topCord = new Coordinate(62.50826064422815,10.733642578125);
+            Coordinate botCord = new Coordinate(64.3755654235413,15.4742431640625);
+            string rawPeaks = await QueryOverpass(peaksQuery, topCord, botCord, log);
+
             RootPeaks myDeserializedClass = JsonSerializer.Deserialize<RootPeaks>(rawPeaks);
             Shared.Peak[] peaks = myDeserializedClass.Elements.Select(x => 
                 new Shared.Peak(x.Id, x.Tags.Ele, x.Tags.Name, x.Tags.NameSma, x.Tags.AltName, new Shared.Point(new double[] {x.Lon, x.Lat}))).ToArray();
@@ -80,5 +79,20 @@ namespace BlazorApp.Api
                 
             return new OkObjectResult("Added " + peaks.Length + " peaks to the database");
         }
+        private static async Task<string> QueryOverpass(string query, Coordinate topCorner, Coordinate botCorner, ILogger log){
+            string bbox = topCorner.lat.ToString(CultureInfo.InvariantCulture) + "," + topCorner.lng.ToString(CultureInfo.InvariantCulture) + "," + 
+                botCorner.lat.ToString(CultureInfo.InvariantCulture) + "," + botCorner.lng.ToString(CultureInfo.InvariantCulture);;
+            var body = @"[out:json][timeout:25];" + "\n" +
+                      $@"            {query}({bbox});" + "\n" +
+                       @"            out body;" + "\n" +
+                       @"            >;" + "\n" +
+                       @"            out skel qt;";
+            var buffer = System.Text.Encoding.UTF8.GetBytes(body);
+            var byteContent = new ByteArrayContent(buffer);
+            var response = await client.PostAsync("https://overpass-api.de/api/interpreter", byteContent);
+            string rawResponseString = await response.Content.ReadAsStringAsync();
+            return rawResponseString;
+        }
     }
+
 }
