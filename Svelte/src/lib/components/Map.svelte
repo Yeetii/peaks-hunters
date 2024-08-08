19bfeceb7ee4a7796d7c4dfde289f817720e77d2
Erik Magnusson <erik.magnusson@forefront.se>
2024-08-08 11:43:00 +0200

    Start of PeaksHunters 2.0, Svelte + MapLibre

---
+++ Svelte/src/lib/components/Map.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { PUBLIC_MAPTILER_KEY } from '$env/static/public';
+	import type { MapStore } from '$lib/stores';
+	import { MAPSTORE_CONTEXT_KEY } from '$lib/stores';
+	import {
+		AttributionControl,
+		GeolocateControl,
+		Map,
+		NavigationControl,
+		ScaleControl
+	} from 'maplibre-gl';
+	import { getContext, onMount } from 'svelte';
+
+	let mapStore: MapStore = getContext(MAPSTORE_CONTEXT_KEY);
+
+	let mapContainer: HTMLDivElement;
+
+	onMount(() => {
+		const map = new Map({
+			container: mapContainer,
+			style: `https://api.maptiler.com/maps/topo-v2/style.json?key=${PUBLIC_MAPTILER_KEY}`,
+			center: [13.0509, 63.41698],
+			zoom: 12,
+			hash: true,
+			attributionControl: false
+		});
+		map.addControl(new NavigationControl({}), 'top-right');
+		map.addControl(
+			new GeolocateControl({
+				positionOptions: { enableHighAccuracy: true },
+				trackUserLocation: true
+			}),
+			'top-right'
+		);
+		map.addControl(new ScaleControl({ maxWidth: 80, unit: 'metric' }), 'bottom-left');
+		map.addControl(new AttributionControl({ compact: true }), 'bottom-right');
+
+		mapStore?.set(map);
+	});
+</script>
+
+<div class="map" data-testid="map" bind:this={mapContainer} />
+
+<style>
+	@import 'maplibre-gl/dist/maplibre-gl.css';
+
+	.map {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+		z-index: 1;
+	}
+</style>
