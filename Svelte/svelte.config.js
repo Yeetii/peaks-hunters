874be26d3f781d6ddc378f768f4949fe83e22546
Erik Magnusson <erik.magnusson@forefront.se>
2024-08-08 13:54:41 +0200

    Use static adapter instead of swa

--- Svelte/svelte.config.js
+++ Svelte/svelte.config.js
@@ -1,4 +1,4 @@
-import azure from 'svelte-adapter-azure-swa';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,15 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: azure()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
