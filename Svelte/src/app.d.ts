cf0a8dde7ad6f097887af7957d8d0605591dc9c6
Erik Magnusson <erik.magnusson@forefront.se>
2024-08-08 12:15:53 +0200

    Switch pipeline to Svelte frontend

--- Svelte/src/app.d.ts
+++ Svelte/src/app.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="svelte-adapter-azure-swa" />
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
@@ -6,4 +7,5 @@ declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
 	// interface Platform {}
+
 }
